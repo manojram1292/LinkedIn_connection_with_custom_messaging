1dbbb58ba45b4569b55ce29762adeef5917e8e0d
manojram1292 <65986464+manojram1292@users.noreply.github.com>
2024-04-26 13:23:49 -0300

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,8 +1,8 @@
 file_name = 'connections.csv'
-linkedin_username = 'aswinarul004@gmail.com'
-linkedin_password = 'Arul004*'
+linkedin_username = '***@gmail.com'
+linkedin_password = '****'
 keywords = 'DevOps,Linux,Python,AWS'
 ignore_list = 'Person1,Person2'
 start_page = 18
 till_page = 1
-custom_message = 'hi, this is asterisk. follow me on linked in by accepting my invite. Thanks in advance.'
+custom_message = 'Hi, this is ****. Follow me on linkedin by accepting my invite. Thanks in advance.'
